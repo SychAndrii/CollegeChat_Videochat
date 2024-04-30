9cc085e0706a0dafbeb520d1b6e97ab1a40bbdaa
SychAndrii <asych@myseneca.ca>
2024-04-30 16:52:25 -0400

    Added core

---
+++ server/src/core/models/Connection.ts
@@ -0,0 +1,11 @@
+class Connection {
+    public name: string;
+    public id: string;
+
+    constructor(name: string, id: string) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+export default Connection;
